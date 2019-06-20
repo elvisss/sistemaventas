5540e796a5ede10e58ea0a069aa38930d1550700
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2019-06-19 23:27:03 -0500

    category crud

---
+++ Sistema.Entidades/Almacen/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Sistema.Entidades.Almacen
+{
+    public class Categoria
+    {
+        public int idcategoria { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre no debe tener más de 50 carácteres ni menos de 3")]
+        public string nombre { get; set; }
+        [StringLength(256)]
+        public string descripcion { get; set; }
+        public bool condicion { get; set; }
+    }
+}
